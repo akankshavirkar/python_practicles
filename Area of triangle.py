aa08accda97e1221b396ce7a8d33dbef1ccd48be
Akanksha Sunil Virkar <102981743+akankshavirkar@users.noreply.github.com>
2022-06-23 23:03:11 +0530

    Delete Area of triangle.py

--- Area of triangle.py
+++
@@ -1,7 +0,0 @@
-#python program To Calculate the Area of Triangle.
-Base = int(input('Enter value of base:'))
-Height =int(input('Enter value of height:'))
-print ('Given:')
-print('Base : {0} Height :{1}'.format(Base,Height))
-Area_of_Triangle =0.5 * Base * Height;
-print ('Area of triangle is {0}'.format(Area_of_Triangle))
